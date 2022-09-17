7ad540c1f3f5820a10c2b22926ee8621271c5251
JadenFicklin <jadendficklin@gmail.com>
2022-09-17 15:58:21 -0600

    added transparent background for sculpture object as well as setting up sections in css

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,6 +8,12 @@ function Home() {
         scene="https://prod.spline.design/1ZSN0NOpjyVsaPIf/scene.splinecode"
         className="sculpture"
       />
+      <div className="home-outer">
+        <div className="left-banner"></div>
+        <div className="right-banner">
+          <div className="green-accent"></div>
+        </div>
+      </div>
     </>
   );
 }
