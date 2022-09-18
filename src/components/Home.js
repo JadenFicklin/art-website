3cb6a9c3135c9295663189d07bd9859443e69a1f
JadenFicklin <jadendficklin@gmail.com>
2022-09-17 19:49:11 -0600

    fixed error

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,10 +4,8 @@ import { BsArrowUpRight } from "react-icons/bs";
 
 function Home() {
   let mouseCursor = document.querySelector(".cursor");
-  let navLinks = document.querySelectorAll(".nav-links li");
 
   window.addEventListener("mousemove", cursor);
-
   function cursor(e) {
     mouseCursor.style.top = e.pageY + "px";
     mouseCursor.style.left = e.pageX + "px";
