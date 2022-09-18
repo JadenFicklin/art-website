85b5bc59356fd87892cc03258337d8d76501147b
JadenFicklin <jadendficklin@gmail.com>
2022-09-17 18:04:56 -0600

    added some styling and text

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,8 +8,127 @@ function Home() {
         scene="https://prod.spline.design/1ZSN0NOpjyVsaPIf/scene.splinecode"
         className="sculpture"
       />
+      <div className="background-text">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Quisque non tellus
+        orci ac auctor augue mauris augue. Vulputate eu scelerisque felis
+        imperdiet proin fermentum leo vel orci. Elit sed vulputate mi sit.
+        Laoreet suspendisse interdum consectetur libero id faucibus nisl
+        tincidunt eget. Magna eget est lorem ipsum dolor sit amet consectetur
+        adipiscing. Facilisis gravida neque convallis a cras semper auctor
+        neque. Porta lorem mollis aliquam ut porttitor leo a diam. Nulla
+        pharetra diam sit amet. Proin nibh nisl condimentum id venenatis a
+        condimentum. Et malesuada fames ac turpis egestas integer eget aliquet
+        nibh. Fames ac turpis egestas integer. Tincidunt vitae semper quis
+        lectus nulla at volutpat diam. Sem fringilla ut morbi tincidunt augue
+        interdum velit euismod. Suspendisse faucibus interdum posuere lorem.
+        Elementum tempus egestas sed sed risus. Sed turpis tincidunt id aliquet.
+        Vitae ultricies leo integer malesuada nunc vel risus commodo. Ut
+        consequat semper viverra nam libero justo laoreet sit amet. Convallis a
+        cras semper auctor neque vitae tempus quam. Faucibus purus in massa
+        tempor nec feugiat nisl pretium. Congue quisque egestas diam in arcu
+        cursus euismod quis. Consectetur adipiscing elit pellentesque habitant
+        morbi. Tincidunt praesent semper feugiat nibh sed pulvinar proin
+        gravida. Diam sollicitudin tempor id eu nisl nunc. A arcu cursus vitae
+        congue mauris. Interdum posuere lorem ipsum dolor. Odio ut sem nulla
+        pharetra diam sit amet. Est velit egestas dui id ornare. Mauris sit amet
+        massa vitae tortor condimentum lacinia quis vel. Turpis tincidunt id
+        aliquet risus feugiat. Magna fringilla urna porttitor rhoncus dolor
+        purus. Adipiscing diam donec adipiscing tristique. In aliquam sem
+        fringilla ut morbi tincidunt. Cras fermentum odio eu feugiat pretium.
+        Dolor sit amet consectetur adipiscing elit pellentesque habitant. Vitae
+        elementum curabitur vitae nunc. Sit amet mauris commodo quis imperdiet
+        massa tincidunt nunc pulvinar. Fames ac turpis egestas maecenas. Tempor
+        orci dapibus ultrices in iaculis nunc sed augue. Eleifend donec pretium
+        vulputate sapien nec sagittis aliquam malesuada bibendum. Nunc id cursus
+        metus aliquam eleifend mi in nulla posuere. Leo integer malesuada nunc
+        vel risus. Dolor morbi non arcu risus quis. Ultrices eros in cursus
+        turpis massa tincidunt dui. Purus viverra accumsan in nisl nisi
+        scelerisque. In ornare quam viverra orci sagittis. Nibh cras pulvinar
+        mattis nunc sed blandit libero volutpat. Aliquam purus sit amet luctus.
+        Turpis massa sed elementum tempus egestas. A diam sollicitudin tempor id
+        eu nisl nunc. Nec dui nunc mattis enim ut tellus. Leo integer malesuada
+        nunc vel risus commodo. Et egestas quis ipsum suspendisse ultrices.
+        Quisque non tellus orci ac auctor augue mauris. Scelerisque felis
+        imperdiet proin fermentum leo vel orci. Non pulvinar neque laoreet
+        suspendisse. Arcu non odio euismod lacinia at quis risus sed. Risus
+        feugiat in ante metus dictum at. Ridiculus mus mauris vitae ultricies
+        leo integer. Nunc non blandit massa enim nec dui nunc mattis enim.
+        Senectus et netus et malesuada fames. In nulla posuere sollicitudin
+        aliquam ultrices sagittis orci. Neque laoreet suspendisse interdum
+        consectetur libero. Quam adipiscing vitae proin sagittis. Accumsan
+        tortor posuere ac ut consequat. Morbi leo urna molestie at elementum eu
+        facilisis sed. Proin sagittis nisl rhoncus mattis. Egestas diam in arcu
+        cursus euismod quis. Netus et malesuada fames ac turpis egestas integer
+        eget. Morbi tempus iaculis urna id volutpat. Sapien nec sagittis aliquam
+        malesuada bibendum arcu. Felis bibendum ut tristique et egestas quis
+        ipsum suspendisse ultrices. Faucibus interdum posuere lorem ipsum dolor
+        sit amet. Leo urna molestie at elementum eu. Id venenatis a condimentum
+        vitae sapien pellentesque habitant morbi tristique. Dolor sit amet
+        consectetur adipiscing elit duis. Suspendisse potenti nullam ac tortor
+        vitae purus faucibus. Ipsum suspendisse ultrices gravida dictum fusce
+        ut. Magna fringilla urna porttitor rhoncus. Tincidunt augue interdum
+        velit euismod. Nunc scelerisque viverra mauris in aliquam sem fringilla
+        ut. Phasellus egestas tellus rutrum tellus pellentesque eu tincidunt.
+        Congue quisque egestas diam in arcu cursus. Dui id ornare arcu odio ut
+        sem nulla pharetra diam. Quis commodo odio aenean sed adipiscing diam
+        donec. Placerat vestibulum lectus mauris ultrices eros in cursus turpis
+        massa. Malesuada fames ac turpis egestas maecenas pharetra convallis.
+        Tristique et egestas quis ipsum. Turpis cursus in hac habitasse platea
+        dictumst quisque sagittis purus. Tellus at urna condimentum mattis
+        pellentesque id. Eget gravida cum sociis natoque penatibus et magnis.
+        Pretium aenean pharetra magna ac placerat vestibulum lectus mauris
+        ultrices. Est sit amet facilisis magna etiam. Nec sagittis aliquam
+        malesuada bibendum arcu vitae elementum. Quis ipsum suspendisse ultrices
+        gravida. Fermentum dui faucibus in ornare. Nec feugiat nisl pretium
+        fusce id. Diam vulputate ut pharetra sit amet aliquam. Nisi porta lorem
+        mollis aliquam ut porttitor. Molestie ac feugiat sed lectus vestibulum.
+        At lectus urna duis convallis convallis tellus id. A pellentesque sit
+        amet porttitor eget dolor morbi non. Consequat ac felis donec et. Ante
+        metus dictum at tempor. Ullamcorper malesuada proin libero nunc. Dui
+        vivamus arcu felis bibendum ut tristique. Tellus molestie nunc non
+        blandit massa enim nec. Turpis in eu mi bibendum. Metus aliquam eleifend
+        mi in nulla posuere sollicitudin aliquam. Parturient montes nascetur
+        ridiculus mus mauris vitae ultricies. Orci eu lobortis elementum nibh
+        tellus molestie nunc non. Ornare arcu dui vivamus arcu. Massa placerat
+        duis ultricies lacus. Augue mauris augue neque gravida in fermentum et.
+        Phasellus egestas tellus rutrum tellus. Sit amet facilisis magna etiam
+        tempor. Bibendum ut tristique et egestas. Felis imperdiet proin
+        fermentum leo vel orci porta. Sagittis nisl rhoncus mattis rhoncus urna
+        neque viverra justo nec. Diam ut venenatis tellus in metus vulputate eu.
+        Aliquam eleifend mi in nulla posuere. Eu mi bibendum neque egestas
+        congue quisque egestas diam. Neque ornare aenean euismod elementum nisi
+        quis eleifend. Quam pellentesque nec nam aliquam sem. Ut morbi tincidunt
+        augue interdum velit. Cursus euismod quis viverra nibh cras pulvinar
+        mattis nunc sed. Felis imperdiet proin fermentum leo vel orci porta non
+        pulvinar. Lorem ipsum dolor sit amet. Est ullamcorper eget nulla
+        facilisi etiam. Rutrum quisque non tellus orci ac auctor. Sit amet risus
+        nullam eget. Arcu bibendum at varius vel pharetra. Netus et malesuada
+        fames ac turpis egestas integer eget aliquet. Massa tincidunt nunc
+        pulvinar sapien et ligula ullamcorper. Arcu ac tortor dignissim
+        convallis aenean et tortor. Magna fermentum iaculis eu non. Sagittis
+        nisl rhoncus mattis rhoncus urna neque viverra justo. Cras tincidunt
+        lobortis feugiat vivamus at. Molestie nunc non blandit massa. Tortor
+        dignissim convallis aenean et. Vulputate ut pharetra sit amet aliquam id
+        diam maecenas ultricies. Risus commodo viverra maecenas accumsan lacus
+        vel.
+      </div>
       <div className="home-outer">
-        <div className="left-banner"></div>
+        <nav className="nav">
+          <div className="logo">Art Gallery</div>
+          <div className="about">About</div>
+          <div className="art">Art</div>
+          <div className="contact-us">Contact Us</div>
+        </nav>
+        <div className="left-banner">
+          <div className="left-inner-banner">
+            <div className="header">Explore Art around the world</div>
+            <div className="sub-header">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt{" "}
+            </div>
+          </div>
+        </div>
         <div className="right-banner">
           <div className="green-accent"></div>
           <div className="blue-accent"></div>
